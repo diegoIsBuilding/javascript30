3a538e8f3560f6e9685250d98b17d277297e630c
gdiego07 <d.garcia07@outlook.com>
2021-01-25 17:37:01 +0000

    review '

--- noise-machine/index.js
+++ noise-machine/index.js
@@ -12,3 +12,23 @@ function playSound(e) {
   key.classList.add("playing");
 }
 window.addEventListener("keydown", playSound);
+
+// function playSound(e) {
+//   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+//   if (!audio) return;
+//   audio.currentTime = 0;
+//   audio.play();
+//   key.classList.add("playing");
+// }
+// function removeTransform(e) {
+//   console.log(e);
+//   //propertyName - does not exist
+//   if (e.propertyName !== "transform") return;
+//   console.log(this);
+//   this.classList.remove("playing");
+// }
+// const keys = document.querySelectorAll(".key");
+// // the removeTransform function is not being called once the sound is played
+// keys.forEach((key) => key.addEventListener("transitionend", removeTransform));
+// window.addEventListener("keydown", playSound);
