5ab2b2cfec0adce5c517f707d86afa7cbc1407d2
gdiego07 <d.garcia07@outlook.com>
2021-02-16 23:23:02 +0000

    fetch exercise redo

--- javscript-fetch/index.js
+++ javscript-fetch/index.js
@@ -1,20 +1,29 @@
-document.addEventListener("DOMCcontentLoaded", () => {
-    const endpoint = "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
-    const cityData = [];
+document.addEventListener("DOMContentLoaded", () => {
 
-    fetch(endpoint)
-    .then((response) => response.json())
-    .then(data => cityData.push(...data));
-
-    function findCityDataMatches (textToMatch, cityData) {
-        // When the user types/inputs a string of a location 
-        // in the form 
-        // the output should be a list of locations matching the 
-        // the input string 
-        return cityData.filter(location => {
-            // filter the location by city OR state
-            const textMatch = new RegExp(textToMatch, 'gi');
-            return location.city(textMatch) || location.state(textMatch);
-        })
-    }
 })
+// shortened the json data url 
+const endpoint = "https://tinyurl.com/j8t8snkp";
+const locationsDataArray = [];
+// fetch the endpoint
+// then turn the raw data into json 
+// then push the json data into the 
+// locationsDataArray
+fetch(endpoint)
+    .then(response => response.json())
+    .then(data => locationsDataArray.push(...data));
+// Check if there is data in the array
+// console.log(locationDataArray);
+function findTextMatch (textToMatch, locationsDataArray) {
+  // check if the search query matches any location in the dataset
+  return locationsDataArray.filter((location) => {
+    // create a dynamic regular expression that checks user input
+    //check location data
+    // run in chrome dev tools
+    const regex = new RegExp(textToMatch, 'gi');
+    return location.city.match(regex) || location.state.match(regex);
+  });
+}
+function displayMatches () {
+    
+}
+
